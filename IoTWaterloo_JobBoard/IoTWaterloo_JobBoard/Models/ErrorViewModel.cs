231ac38f02d12e738f657f78222e332acc43f197
AnjaliParikh1606 <56400186+AnjaliParikh1606@users.noreply.github.com>
2019-10-25 15:26:45 -0400

    WebUI Design
    Create New Project and  Designed share_layout page

---
+++ IoTWaterloo_JobBoard/IoTWaterloo_JobBoard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IoTWaterloo_JobBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
